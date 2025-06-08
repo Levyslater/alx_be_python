d5123329d27fbdb010fcc4e54a834750605139a2
Levyslater <muthui@yandex.com>
2025-06-08 21:29:31 +0300

    fixed date format in time module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print(current_date.strftime("%Y-%m-%d %I:%M:%S"))
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to calculate: "))
