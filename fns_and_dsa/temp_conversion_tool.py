dddc9e833d3ad954bd46ccfeb42e4a7799016329
Levyslater <muthui@yandex.com>
2025-06-08 23:19:56 +0300

    removed paranthesis in global variables

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,8 @@
 """
 This script will define functions to convert temperatures between Celsius and Fahrenheit
 """
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def main():
     def convert_to_celsius(fahrenheit):
