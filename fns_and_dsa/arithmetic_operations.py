6376930dba8ec75f539e5325423d0edf75844b6b
Levyslater <muthui@yandex.com>
2025-06-08 17:43:44 +0300

    used if else instead of match

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 """ Performs basic arithmetic operations """
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            return num1 / num2 if num2 != 0 else "Cannot devide by Zero!"
+
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        return num1 / num2 if num2 != 0 else "Cannot devide by Zero!"
