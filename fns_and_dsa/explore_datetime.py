f6b23e2f63a13bbacd3e8fdbbe8904f77f001f6a
Levyslater <muthui@yandex.com>
2025-06-08 21:31:55 +0300

    rephrased the days request prompt in time module

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
     print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 def calculate_future_date():
-    number_of_days = int(input("Enter the number of days to calculate: "))
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
     future_date = datetime.now() + timedelta(days=number_of_days)
     print(future_date.strftime("%Y-%m-%d"))
 
