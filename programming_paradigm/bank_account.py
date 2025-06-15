08a63854a0fa2d010f2b36288c47405c7b44a33a
Levyslater <muthui@yandex.com>
2025-06-15 19:08:55 +0300

    convert balance into float type

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,17 +4,17 @@ BankAccount class that encapsulates banking operations.
 """
 class BankAccount:
     def __init__(self, current_balance):
-        self.current_balance = current_balance
+        self._current_balance = float(current_balance)
     def deposit(self, amount):
         if amount >= 0:
-            self.current_balance += amount
+            self._current_balance += amount
         else:
             pass
     def withdraw(self, amount):
-        if self.current_balance >= amount and amount > 0:
-            self.current_balance -= amount
+        if self._current_balance >= amount and amount > 0:
+            self._current_balance -= amount
             return True
         else:
             return False
     def display_balance(self):
-        print(f"Current Balance: ${self.current_balance}")
+        print(f"Current Balance: ${self._current_balance:.2f}")
